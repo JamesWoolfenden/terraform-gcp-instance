ab051438c1b0e29b7c3900674e3c182881199e08
James Woolfenden <james.woolfenden@gmail.com>
2026-06-11 23:58:32 +0100

    refactor(examplea): name GCE service agent via local for clarity

--- example/examplea/google_kms_crypto_key_iam_member.tf
+++ example/examplea/google_kms_crypto_key_iam_member.tf
@@ -2,8 +2,12 @@ data "google_project" "this" {
   project_id = var.project
 }
 
+locals {
+  gce_service_agent = "serviceAccount:service-${data.google_project.this.number}@compute-system.iam.gserviceaccount.com"
+}
+
 resource "google_kms_crypto_key_iam_member" "compute_encrypter_decrypter" {
   crypto_key_id = google_kms_crypto_key.instance.id
   role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
-  member        = "serviceAccount:service-${data.google_project.this.number}@compute-system.iam.gserviceaccount.com"
+  member        = local.gce_service_agent
 }
